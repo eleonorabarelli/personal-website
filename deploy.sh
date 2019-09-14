3911d5d527283f8e6316b00a19664b9bf954a67a
eleonorabarelli <eleonora.barelli2@unibo.it>
2019-09-14 18:15:39 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ rm -rf public
 mkdir public
 cp -r css index.html favicon.ico public/
 
-# deploy
-cd public
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:master > /dev/null 2>&1
+if [ "${TRAVIS_BRANCH}" == "master" ]; then
+  # deploy
+  cd public
+  git init
+  git add .
+  git commit -m "Deploy to Github Pages"
+  git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:master > /dev/null 2>&1
+fi
